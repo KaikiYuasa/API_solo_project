107229b147f3834e601b5863059c87eb949d41dd
湯浅海貴 <yuasa.kshu@gmail.com>
2020-11-13 20:20:39 +0900

    fix frontend(post,patch,delete)

--- requestApi.js
+++ requestApi.js
@@ -1,16 +1,69 @@
 
 function requestAPI(){
 
-    console.log(form.input1.value);
-    fetch('http://localhost:3001/api/' + form.input1.value)
-    .then((response) => {
-        return response.json()
-    })
-    .then((result) => {
-        console.log(JSON.stringify(result,null,2));
-        document.getElementById('span1').textContent = JSON.stringify(result,null,2);
-        return result;
-    })
+    const num = form.typeSelect.selectedIndex;
+    const str = form.typeSelect.options[num].value;
+
+    if(str =="GET"){
+        document.getElementById('span1').textContent = "";
+        fetch('http://localhost:3001/api/' + form.input1.value)
+        .then((response) => {
+            return response.json()
+        })
+        .then((result) => {
+            document.getElementById('span1').textContent = JSON.stringify(result,null,2);
+            return result;
+        })
+    } else if (str == "POST"){
+        const method = "POST";
+        const headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          };
+        const body = JSON.stringify(JSON.parse(form.input2.value));
+        document.getElementById('span1').textContent = "";
+        fetch('http://localhost:3001/api/' + form.input1.value,{
+            method,headers,body
+        })
+        .then((response) => {
+            return response.json()
+        })
+        .then((result) => {
+            document.getElementById('span1').textContent = JSON.stringify(result,null,2);
+            return result;
+        })
+    }else if (str == "PATCH"){
+        const method = 'PATCH';
+        const headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          };
+        const body = JSON.stringify(JSON.parse(form.input2.value));
+        document.getElementById('span1').textContent = "";
+        fetch('http://localhost:3001/api/' + form.input1.value,{
+            method,headers,body
+        })
+        .then((response) => {
+            return response.json()
+        })
+        .then((result) => {
+            document.getElementById('span1').textContent = JSON.stringify(result,null,2);
+            return result;
+        })
+    } else if (str == "DELETE"){
+        const method = 'DELETE';
+        document.getElementById('span1').textContent = "";
+        fetch('http://localhost:3001/api/' + form.input1.value,{
+            method
+        })
+        .then((response) => {
+            return response.json()
+        })
+        .then((result) => {
+            document.getElementById('span1').textContent = JSON.stringify(result,null,2);
+            return result;
+        })
+    }
 }
 
 function display(){
